eb8ddbaed31bbfae29d9d7b50edd6502834809d7
faisaljaweed <faisaljawed2000@gmail.com>
2024-10-11 22:05:55 +0500

    video controller almost complete

--- src/controllers/video.conroller.js
+++ src/controllers/video.conroller.js
@@ -1,6 +1,8 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asynchandler } from "../utils/asynchandler.js";
-import { Video } from "../models";
+import { Video } from "../models/video.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { isValidObjectId } from "mongoose";
 // get all video get
 // Publish/ Add video post
 // delete video delete
@@ -14,22 +16,133 @@ const getAllVIdeos = asynchandler(async (req, res) => {
 
 const publishAVideo = asynchandler(async (req, res) => {
   const { title, description } = req.body;
+  if (!(title, description, req.files)) {
+    throw new ApiError(402, "title, description and video files are requires");
+  }
+
+  if (!req.files.videofile || req.files.videofile === 0) {
+    throw new ApiError(402, "Video is Already Exists");
+  }
+
+  if (!req.files.thumbnail) {
+    throw new ApiError(401, "Thumbnail is requird");
+  }
+
+  const videolocalfile = req.files.videofile[0].path;
+  const thumbnaillocalfile = req.files.thumbnail[0].path;
+
+  if (!videolocalfile) {
+    throw new ApiError(401, "video local is required");
+  }
+
+  if (!thumbnaillocalfile) {
+    throw new ApiError(401, "video local is required");
+  }
+  const videofile = await uploadOnCloudinary(videolocalfile);
+  const thumbnail = await uploadOnCloudinary(thumbnaillocalfile);
+  if (!videofile) {
+    throw new ApiError(401, "video is missing in cloudanry");
+  }
+  if (!thumbnail) {
+    throw new ApiError(401, "thumbnail is missing in cloudinary");
+  }
+
+  const newVideo = await Video.create({
+    videofile: videofile.url,
+    thumbnail: thumbnail.url,
+    title,
+    description,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, newVideo, "Video Succesfully upload"));
 });
 
 const getVideobyId = asynchandler(async (req, res) => {
   const { videoId } = req.params;
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(401, "Video is is not valid");
+  }
+  const getVideo = await Video.findById(videoId).populate("owner", "name");
+  if (!getVideo) {
+    throw new ApiError(401, "Video not found");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, getVideo, "video fetched successfully"));
 });
 
 const updateVideo = asynchandler(async (req, res) => {
   const { videoId } = req.params;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(401, "Video id is not Valid");
+  }
+
+  const video = Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(401, "Video not found");
+  }
+
+  const { title, description, thumbnail, videofile } = req.body;
+
+  if (title) {
+    video.title = title;
+  }
+  if (description) {
+    video.description = description;
+  }
+  if (thumbnail) {
+    video.thumbnail = thumbnail;
+  }
+  if (videofile) {
+    video.videofile = videofile;
+  }
+  await video.save();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video Update Successfully"));
 });
 
 const deleteVideo = asynchandler(async (req, res) => {
   const { videoId } = req.params;
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(401, "video id is not valid");
+  }
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(401, "Video not found");
+  }
+  await Video.findByIdAndDelete(videoId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Video Successfully Deleted"));
 });
 
 const togglePublishStatus = asynchandler(async (req, res) => {
   const { videoId } = req.params;
+  if (isValidObjectId(videoId)) {
+    throw new ApiError(401, "thsi video is not valid");
+  }
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(401, "Video not found");
+  }
+  video.ispublished = ispublished;
+  await video.save();
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        video,
+        `video published status updated to ${video.ispublished ? "published" : "unpublished"}`
+      )
+    );
 });
 
 export {
